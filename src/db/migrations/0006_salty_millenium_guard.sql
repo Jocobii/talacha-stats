0aec20215bb685f6adc60376583359ef9339f8fa
Alexander Jocobi <adalbertojocobi@gmail.com>
2026-04-21 16:06:34 -0700

    fix: migrations

--- src/db/migrations/0006_salty_millenium_guard.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "competitions" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "competitions" CASCADE;--> statement-breakpoint
-ALTER TABLE "leagues" DROP CONSTRAINT "leagues_competition_id_competitions_id_fk";
---> statement-breakpoint
-ALTER TABLE "leagues" ALTER COLUMN "day_of_week" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "leagues" DROP COLUMN "competition_id";--> statement-breakpoint
-ALTER TABLE "leagues" DROP COLUMN "status";--> statement-breakpoint
-ALTER TABLE "leagues" DROP COLUMN "start_date";--> statement-breakpoint
-ALTER TABLE "leagues" DROP COLUMN "end_date";
